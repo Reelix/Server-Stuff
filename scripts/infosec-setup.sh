ad2c651be2819c1c00227f0f3983447a7f44c2c1
Reelix <reelix+github@gmail.com>
2020-12-15 05:42:57 +0200

    Create infosec-setup.sh

---
+++ scripts/infosec-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# For connecting to VPNs
+sudo apt install openvpn
+# For scanning
+sudo apt install nmap
+# For fixing nc connections
+sudo apt install rlwrap
+# For proper color
+sudo apt install grc
+# SQLMap
+sudo apt install sqlmap
