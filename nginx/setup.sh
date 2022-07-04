6dd2c01a4dd51cfd6d8f9ba788ec7ab4d73f91fe
Reelix <reelix+github@gmail.com>
2022-07-04 16:41:27 +0200

    Update setup.sh
    - Updated php version
    - More secure rsa key for random testing sites

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -2,8 +2,9 @@
 # nginx
 sudo apt install nginx
 # php
-sudo apt install php8.0-fpm
+sudo apt install php8.1-fpm
 # certbot
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 echo "Fix DNS (freedns), then run: sudo certbot --nginx"
+echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096"
