e688a329ef529b82afeb422b1080c27655a23bde
Reelix <reelix+github@gmail.com>
2022-12-12 19:34:18 +0200

    Update setup.sh
    Fixed OSCP stapling not working

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -6,5 +6,5 @@ sudo apt install php8.1-fpm
 # certbot
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-echo "Fix DNS (freedns), then run: sudo certbot --nginx"
-echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096"
+echo "Fix DNS (freedns), then run: sudo certbot --nginx --must-staple --staple-ocsp"
+echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096 --must-staple --staple-ocsp"
