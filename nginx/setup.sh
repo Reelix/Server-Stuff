f0d70558c21c9cc244bd4e516f4e48c0216850e6
Reelix <reelix+github@gmail.com>
2024-04-29 08:40:50 +0200

    Update setup.sh

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -2,9 +2,8 @@
 # nginx
 sudo apt install nginx
 # php
-sudo apt install php8.1-fpm
+sudo apt install php8.3-fpm
 # certbot
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo apt install python3-certbot python3-certbot-nginx
 echo "Fix DNS (freedns), then run: sudo certbot --nginx --must-staple --staple-ocsp"
 echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096 --must-staple --staple-ocsp"
