72d0b9eedcdabb1e8a566decac414adc4fd909f7
Reelix <reelix+github@gmail.com>
2025-07-30 22:16:50 +0200

    Update setup.sh - Key Exchange Fix
    For that 100% Key Exchange on https://www.ssllabs.com/ssltest/

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -6,4 +6,4 @@ sudo apt install php8.3-fpm
 # certbot
 sudo apt install python3-certbot python3-certbot-nginx
 echo "Fix DNS (freedns), then run: sudo certbot --nginx --must-staple --staple-ocsp"
-echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096 --must-staple --staple-ocsp"
+echo "Then /usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --key-type ecdsa --elliptic-curve secp384r1"
