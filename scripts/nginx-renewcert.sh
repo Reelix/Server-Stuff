9eda9d99c77c6988f36b418e1c7f77ede9352afd
Reelix <reelix+github@gmail.com>
2026-03-16 15:57:18 +0200

    Update nginx-renewcert.sh

--- scripts/nginx-renewcert.sh
+++ scripts/nginx-renewcert.sh
@@ -1,5 +1,5 @@
 # Nginx
-/usr/bin/sudo /usr/bin/certbot renew -q --nginx --force-renew --rsa-key-size 4096 --must-staple --staple-ocsp
+/usr/bin/sudo /usr/bin/certbot renew -q --nginx --key-type ecdsa --elliptic-curve secp384r1 --staple-ocsp --force-renewal
 
 # Save Certs locally
-/usr/bin/sudo /usr/bin/certbot renew --force-renew --rsa-key-size 4096 --staple-ocsp -w /home/reelix/web
+/usr/bin/sudo /usr/bin/certbot certonly --webroot --key-type ecdsa --elliptic-curve secp384r1 --staple-ocsp --force-renewal -w /home/reelix/web
